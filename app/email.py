a2a95a4c2312788ea20b843e8341fab74fbbfdc1
Blake Montgomery <blakemontgomery312@gmail.com>
2021-10-25 14:58:57 -0500

    Add more text to emails

--- app/email.py
+++ app/email.py
@@ -1,11 +1,21 @@
+from _typeshed import SupportsItemAccess
 from flask_mail import Message
 from app import app, mail
 
 
 def send_resume(recipient):
     msg = Message('You requested a resume?', recipients=[recipient])
-    msg.body = ('Here is your resume. Thanks!')
-    msg.html = ('<h1>Here is your resume.</h1> <p>Thanks!</p>')
+    msg.body = ('Here is your resume. Thanks!'
+                'Blake Montgomery.'
+                'software engineer | full-stack web development'
+                '__________________________________________________'
+                'proficiences'
+                'ReactJS, vanilla JavaScript, HTML5, CSS/SCSS'
+                'Python, Flask, SQLAlchemy, SQLite, PodstgreSQL'
+                'NodeJS, Express, Mongoose, MongoDB'
+                'Ruby on Rails, Git'
+                )
+    msg.html = ('<p>Here is your resume. Thanks!</p> <h1>Blake Montgomery</h1> <h2>software engineer | full-stack web development</h2> <hr /> <h3>proficiences</h3> <ul> <li>ReactJS, vanilla JavaScript, HTML5, CSS/SCSS</li> <li>Python, Flask, SQLAlchemy, SQLite, PostgreSQL</li> <li>NodeJS, Express, Mongoose, MongoDB</li> <li>Ruby on Rails, Git</li>')
     
     with app.open_resource("static/resources/blake_montgomery_resume_092421.pdf") as bm_resume:
         msg.attach("static/resources/blake_montgomery_resume_092421.pdf", "text/pdf", bm_resume.read())
