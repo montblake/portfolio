ea00a6333c5be2933dcdc1fb3703d1f52fdbe831
Blake Montgomery <blakemontgomery312@gmail.com>
2021-10-25 12:19:11 -0500

    Change Email Settings

--- app/email.py
+++ app/email.py
@@ -4,8 +4,8 @@ from app import app, mail
 
 def send_resume(recipient):
     msg = Message('You requested a resume?', recipients=[recipient])
-    msg.body = ('Here is your resume. Thanks, ' + recipient)
-    msg.html = ('<h1>Here is your resume.</h1> <p>Thanks, ' + recipient + '!</p>')
+    msg.body = ('Here is your resume. Thanks!'
+    msg.html = ('<h1>Here is your resume.</h1> <p>Thanks!</p>')
     
     with app.open_resource("static/resources/blake_montgomery_resume_092421.pdf") as bm_resume:
         msg.attach("static/resources/blake_montgomery_resume_092421.pdf", "text/pdf", bm_resume.read())
